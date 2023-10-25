5854705ce083fe0541d82d4bd91919c13a872a24
samina-shameem <samina.shameem.clt@gmail.com>
2023-10-25 11:40:00 +0200

    updated test 2.1

--- answer.js
+++ answer.js
@@ -211,12 +211,42 @@ dbwebb.assert("1.4", ANSWER, false);
  * Write your code below and put the answer into the variable ANSWER.
  */
 
-let myFruit
+
+let myFruit="plum";
+let message;
+let color;
+switch (myFruit){
+    case "banana":
+        // message = "The banana is yellow.";
+        // console.log(message);
+        color = "yellow";
+        break;
+    case "apple":
+        // message = "The apple is green.";
+        // console.log(message);
+        color = "green";
+        break;
+    case "kiwi":
+        // message = "The kiwi is green.";
+        // console.log(message);
+        color = "green";        
+        break;
+    case "plum":
+        // message = "The plum is purple.";
+        // console.log(message);
+        color = "purple";
+        break;
+    default:
+        // message = "I don't know this fruit.";
+        // console.log(message);   
+        color = "not defined";
+}
 
 
 
+message = `The ${myFruit} is ${color}.`;
 
-ANSWER = "Replace this text with the variable holding the answer.";
+ANSWER = message;
 
 // I will now test your answer - change false to true to get a hint.
 dbwebb.assert("2.1", ANSWER, false);
